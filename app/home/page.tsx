13aa7f27cb0e040f25b3cb9e981ebbe2c5488c90
MattMark <matthew99729@gmail.com>
2026-04-17 20:31:33 -0700

    Added cool glow

--- app/home/page.tsx
+++ app/home/page.tsx
@@ -11,7 +11,9 @@ export default function Home(){
     return (
         <div>
             <button onClick={() => handleInput("Okay Just gonna test this real quick", "This is my output", 40)}>
-                <pre className={"text-left text-xl drop-shadow-[0_0_8px_rgba(255,0,0,0.7)] font-mono text-green-400 shadow-green-800"}>{buffer}{blink || isTyping ? "█" : ""}</pre>
+                <pre className={"text-glow text-left text-3xl text-green-400 shadow-green-800"}>
+                    {buffer}{blink || isTyping ? "█" : ""}
+                </pre>
             </button>
         </div>
     );
